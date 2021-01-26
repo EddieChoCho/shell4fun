f68aa735ea8654897bbaf3d18903d4bd7d7540d1
EddieChoCho <chochoforwork@gmail.com>
2021-01-26 20:38:56 +0800

    Cooperate with SMHI Open Data API

--- change-default-shell-prompt/README.md
+++ change-default-shell-prompt/README.md
@@ -1,2 +1,15 @@
+# Change the default shell prompt
+
+## Description
+Shows the weather icon of the current weather in Stockholm. 
+
+## Install dependencies
+```
+brew install curl jq
+```
+
+## References
 * [How to change the default shell prompt.](https://access.redhat.com/solutions/505983)
+* [jq - a lightweight and flexible command-line JSON processor.](https://stedolan.github.io/jq/)
+* [SMHI Open Data API](https://opendata.smhi.se/apidocs/)
 * [fish-weather-icon-prompt - by Mousaka](https://github.com/Mousaka/fish-weather-icon-prompt)
--- change-default-shell-prompt/shell_prompt.sh
+++ change-default-shell-prompt/shell_prompt.sh
@@ -1,23 +1,94 @@
 #!/bin/sh
 
 weather_icon(){
-  weather_code=$((1 + $RANDOM % 4))
-  echo "$(_emoji_weather $weather_code)"
+    weather_code="$(curl -s https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.063240/lat/59.334591/data.json | jq '.timeSeries[0].parameters[] | select(.name | contains ("Wsymb2")) | .values[0]')"
+    echo "$(_emoji_weather $weather_code)"
 }
 
 _emoji_weather(){
     case ${1} in
-       1)
-        echo ☀️
-	;;
-       2)
-        echo ⛅️
-	;;
-       3)
-        echo 🌧
-	;;
-       4)
-        echo ☁️
-	;;
+        1)
+            echo ☀️
+            ;;
+        2)
+            echo 🌤
+            ;;
+        3)
+            echo ⛅️
+            ;;
+        4)
+            echo 🌥
+            ;;
+        5)
+            echo ☁️
+            ;;
+        6)
+            echo ☁️
+            ;;
+        7)
+            echo 🌫
+            ;;
+        8)
+            echo 🌧
+            ;;
+        9)
+            echo 🌧🌧
+            ;;
+        10)
+            echo 🌧🌧
+            ;;
+        11)
+            echo ⛈
+            ;;
+        12)
+            echo 🌨
+            ;;
+        13)
+            echo 🌨
+            ;;
+        14)
+            echo 🌨🌨
+            ;;
+        15)
+            echo 🌨
+            ;;
+        16)
+            echo 🌨🌨
+            ;;
+        17)
+            echo 🌨🌨
+            ;;
+        18)
+            echo 🌧
+            ;;
+        19)
+            echo 🌧🌧
+            ;;
+        20)
+            echo 🌧🌧
+            ;;
+        21)
+            echo ⛈
+            ;;
+        22)
+            echo 🌨
+            ;;
+        23)
+            echo 🌨🌨
+            ;;
+        24)
+            echo 🌨🌨
+            ;;
+        25)
+            echo 🌨
+            ;;
+        26)
+            echo 🌨🌨
+            ;;
+        27)
+            echo 🌨🌨
+            ;;
+        *)
+            echo ❔
     esac
 }
